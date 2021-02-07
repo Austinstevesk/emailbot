fcc001cf08b1167c5b5a3225f5a5c49abd2c6e10
Austinstevesk <austinstevesk@gmail.com>
2021-02-07 21:01:40 +0300

    Initial commit

--- emailbot.py
+++ emailbot.py
@@ -1,11 +1,25 @@
 import smtplib
+import speech_recognition as sr
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login('exampleemail@gmail.com', 'examplepass') #Ensure you have your mail in an environment variable for security reason
-server.sendmail(
-    'exampleemail@gmail.com', #From
-    'exampleemail2@gmail.com', #To
-    'Hello, join the party today' #Body
+listener = sr.Recognizer()
 
-)
+try:
+    with sr.Microphone() as source: 
+        print('Listening...')
+        voice = listener.listen(source)
+        info = listener.recognize_google(voice)
+        print(info)
+
+except:
+    pass
+
+def  send_mail():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login('exampleemail@gmail.com', 'examplepass') #Ensure you have your mail in an environment variable for security reason
+    server.sendmail(
+        'exampleemail@gmail.com', #From
+        'exampleemail2@gmail.com', #To
+        'Hello, join the party today' #Body
+
+    )
